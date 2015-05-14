945c2895327550c9605696b9d6154035be996f17
Khairul Akmal <mofodox@mofodox.me>
2015-05-14 23:57:48 +1000

    Load pins in index action by reversing chronological order

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -6,7 +6,7 @@ class PinsController < ApplicationController
   # GET /pins
   # GET /pins.json
   def index
-    @pins = Pin.all
+    @pins = Pin.all.order("created_at DESC")
   end
 
   # GET /pins/1
